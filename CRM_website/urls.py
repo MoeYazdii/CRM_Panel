3065275e0a5a10ae3d32759b2de1d99b3eddc423
moe <moeyazdii@gmail.com>
2024-12-23 10:03:45 +0330

    Final Project v1

--- CRM_website/urls.py
+++ CRM_website/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('register',views.register_user, name='register'),       
     path('record/<int:pk>', views.customer_record,name='record'),
     #<int:pk>  int as a input and this int is a Primary Key
+    path('delete_record/<int:pk>', views.delete_record,name='delete_record'),
+    path('add_record/', views.add_record,name='add_record'),
+    path('update_record/<int:pk>', views.update_record,name='update_record'),
 ]
